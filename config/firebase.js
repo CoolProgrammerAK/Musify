5ab60769681b6f7220854d1a31b94abc8aba1ec0
CoolProgrammerAK <avineykhetarpal01@gmail.com>
2021-06-06 12:25:06 +0530

    update firebase details

--- config/firebase.js
+++ config/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBln90SvTC21FHwm8e1FaSJmxr2wc7BeWs",
-    authDomain: "spotify-976db.firebaseapp.com",
-    databaseURL: "https://spotify-976db.firebaseio.com",
-    projectId: "spotify-976db",
-    storageBucket: "spotify-976db.appspot.com",
-    messagingSenderId: "666324277607",
-    appId: "1:666324277607:web:1d0e3746ab1e029b50b7c2"
+    apiKey: "xxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   };
 
   firebase.initializeApp(firebaseConfig)
